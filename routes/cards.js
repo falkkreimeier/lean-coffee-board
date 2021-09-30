01e0ac811ee9b6853ae2c47a89a024516fc2f971
Falk Kreimeier <f.kreimeier@gmx.net>
2021-09-30 09:19:42 +0200

    initialize Server and bring the new Mongo in it

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Card = require('../models/Card')
 const { nanoid } = require('nanoid')
 
 const router = express.Router()
@@ -17,73 +18,38 @@ let cards = [
 ]
 
 router.get('/', (request, response) => {
-  response.status(200).json(cards)
+  Card.find()
+    .then(data => response.status(200).json(data))
+    .catch(error => response.status(404).json(error))
 })
 
 router.get('/:id', (request, response) => {
   const { id } = request.params
-  // const params = request.params // { id: '1234abc' }
-  // const id = params.id // '1234abc'
+  Card.findById(id)
+    .then(data => response.status(200).json(data))
+    .catch(error => response.status(404).json(error))
 
-  const card = cards.find(card => card.id === id)
-  // card === undefined: falsy
-  // card === {...}: truthy
-  if (card) {
-    response.status(200).json(card)
-  } else {
-    const error = { message: 'Could not find object with that id.' }
-    response.status(404).json(error)
-  }
-
-  // if(!card) {
+  // const card = cards.find(card => card.id === id)
+  // if (card) {
+  //   response.status(200).json(card)
+  // } else {
   //   const error = { message: 'Could not find object with that id.' }
-  //   return response.status(404).json(error)
+  //   response.status(404).json(error)
   // }
-
-  // return response.status(200).json(card)
 })
 
 router.post('/', (request, response) => {
-  const { text, author } = request.body // { text: "What is node?", author: "Max M." }
+  const { text, author } = request.body
 
   if (text === '' && author === '') {
     const error = { message: 'Information missing.' }
-    return response.status(400).json(error) // Bad Request
+    return response.status(400).json(error)
   }
+  const newCard = { text, author }
 
-  const newCard = { text, author, id: nanoid() } // { text: text, author: author, id: nanoid() }
-  cards = [...cards, newCard]
-  // cards.push(newCard)
-  response.status(200).json(newCard)
-})
-
-router.put('/:id', (request, response) => {
-  // PUT: gesamtes Objekt wird mit den Daten im request.body geupdated
-  // PUT request handler implementieren
-  const { id } = request.params
-  const { text, author } = request.body
-
-  if (text === '' || author === '') {
-    const error = { message: 'Information missing.' }
-    return response.status(400).json(error) // Bad Request
-  }
-
-  const card = cards.find(card => card.id === id)
-
-  if (!card) {
-    const error = { message: 'Could not find object with that id.' }
-    return response.status(404).json(error)
-  }
-  const newCard = {
-    text: text ? text : '',
-    author: author ? author : '',
-    id: card.id,
-  }
-
-  const index = cards.findIndex(card => card.id === id)
-
-  cards = [...cards.slice(0 - index), newCard, ...cards.slice(index + 1)]
-  response.status(200).json(newCard)
+  Card.create(newCard)
+    .then(newCard => response.status(201).json(newCard))
+    .catch(error => response.status(404).json(error))
 })
 
 router.patch('/:id', (request, response) => {
@@ -91,46 +57,46 @@ router.patch('/:id', (request, response) => {
   const { text, author } = request.body
 
   if (!text && !author) {
-    // wenn text UND auhtor beide leer sind, dann schmeiß einen Error
     const error = { message: 'Information missing.' }
-    return response.status(400).json(error) // Bad Request
+    return response.status(400).json(error)
   }
 
-  const card = cards.find(card => card.id === id)
+  Card.findByIdAndUpdate(id, { text, author }, { new: true }).then(card =>
+    response
+      .status(200)
+      .json(card)
+      .catch(error => response.status(400).json(error))
+  )
 
-  if (!card) {
-    const error = { message: 'Could not find object with that id.' }
-    return response.status(404).json(error)
-  }
+  // const card = cards.find(card => card.id === id)
 
-  const newCard = {
-    text: text ? text : card.text,
-    author: author ? author : card.author,
-    id: card.id,
-  }
+  // if (!card) {
+  //   const error = { message: 'Could not find object with that id.' }
+  //   return response.status(404).json(error)
+  // }
 
-  const index = cards.findIndex(card => card.id === id) // gives index of card
+  // const newCard = {
+  //   text: text ? text : card.text,
+  //   author: author ? author : card.author,
+  //   id: card.id,
+  // }
 
-  cards = [...cards.slice(0, index), newCard, ...cards.slice(index + 1)]
+  // const index = cards.findIndex(card => card.id === id) // gives index of card
 
-  // const firstHalfOfArray = cards.slice(0, index)
-  // const lastHalfofArray = cards.slice(index + 1)
+  // cards = [...cards.slice(0, index), newCard, ...cards.slice(index + 1)]
 
-  // cards = [...firstHalfOfArray, newCard, ...lastHalfofArray]
-  response.status(200).json(newCard)
+  // // const firstHalfOfArray = cards.slice(0, index)
+  // // const lastHalfofArray = cards.slice(index + 1)
+
+  // // cards = [...firstHalfOfArray, newCard, ...lastHalfofArray]
+  // response.status(200).json(newCard)
 })
 
 router.delete('/:id', (request, response) => {
   const { id } = request.params
-  const card = cards.find(card => card.id === id)
-
-  if (card) {
-    cards = cards.filter(card => card.id !== id)
-    response.status(200).json(card)
-  } else {
-    const error = { message: 'Could not find object with that id.' }
-    response.status(404).json(error)
-  }
+  Card.findByIdAndDelete(id)
+    .then(card => response.status(200).json(card))
+    .catch(error => response.statius(404).json(error))
 })
 
 module.exports = router
